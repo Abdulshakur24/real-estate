4786b6e872791122a7634f9d63d181fef84f1d1e
abdulshakur <abdulshakur486@gmail.com>
2021-12-16 16:44:48 +0300

    corrected misspelling env var.

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -4,7 +4,7 @@ export const baseUrl = "https://bayut.p.rapidapi.com";
 export const fetchApi = async (url) => {
   const { data } = await axios.get(url, {
     headers: {
-      "x-rapidapi-host": process.env.RAPIDNEXT_PUBLIC_RAPID_API_HOST_API_HOST,
+      "x-rapidapi-host": process.env.NEXT_PUBLIC_RAPID_API_HOST,
       "x-rapidapi-key": process.env.NEXT_PUBLIC_RAPID_API_KEY,
     },
   });
